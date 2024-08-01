cc2612774c8985366716cc7f95cd821bc359a4bb
Brian <bw49078@gmail.com>
2024-08-01 14:02:53 -0500

    fixed overflow bug on main div

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-xl w-full items-center justify-between font-mono text-sm ">
         <h1 className="text-5xl text-center">Ai Pantry Tracker</h1>
-        <div className="bg-gray-800 items-center rounded-lg mt-6 h-20 min-h-fit">
-          <form className="grid grid-cols-5 grid-rows-1 gap-4 p-5 ">
+        <div className="bg-gray-800 items-center rounded-lg mt-6 min-h-fit ">
+          <form className="grid grid-cols-5 grid-rows-1 gap-4 p-5 m-0">
             <input 
             className="col-span-2 h-10 font-bold p-3" 
             placeholder="Item Name"
@@ -22,12 +22,17 @@ export default function Home() {
             />
             <button className="col-start-5 bg-slate-950 h-11 text-2xl p-1">+</button>
           </form>
-          <div className="grid grid-cols-5 grid-rows-5 gap-4 p-5 ">
+          <div className="my-0 ">
             {items.map((item) => (
-              <div className="col-span-4 h-10 font-bold p-3">
-                {item.name}
-              </div>
-            ))}
+              <ul>
+                <li>
+                  <div>
+                    <span>{item.name}</span>
+                    <span>{item.quantity}</span>
+                  </div>
+                </li>
+              </ul>
+              ))}
           </div>
         </div>
       </div>

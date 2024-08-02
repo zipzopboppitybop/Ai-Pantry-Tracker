9c51f957d41f31def36d574afffe7e2b8fd6fcb0
Brian <bw49078@gmail.com>
2024-08-02 16:01:40 -0500

    added use state for item name and quantity

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
-
+import { useState } from "react";
+const [itemName, setitemName] = useState("");
+const [itemQuantity, setitemQuantity] = useState(0);
 
 const items = [
   {name: "Milk", quantity: 1},
@@ -13,19 +15,19 @@ export default function Home() {
         <div className="bg-gray-800 items-center rounded-lg mt-6 min-h-fit ">
           <form className="grid grid-cols-5 grid-rows-1 gap-4 p-5 m-0">
             <input 
-            className="col-span-2 h-10 font-bold p-3" 
+            className="col-span-2 h-10 font-bold rounded-lg p-3" 
             placeholder="Item Name"
             />
             <input 
-            className="col-span-2 col-start-3 font-bold h-10 p-3"
+            className="col-span-2 col-start-3 font-bold rounded-lg  h-10 p-3"
             placeholder="Quantity" 
             />
-            <button className="col-start-5 bg-slate-950 h-11 text-2xl p-1">+</button>
+            <button className="col-start-5 bg-slate-950 rounded-lg  h-11 text-2xl p-1">+</button>
           </form>
           <div className="my-0 ">
-            {items.map((item) => (
+            {items.map((item, id) => (
               <ul>
-                <li>
+                <li key={id} className="pb-2 px-5 text-lg">
                   <div>
                     <span>{item.name}</span>
                     <span>{item.quantity}</span>

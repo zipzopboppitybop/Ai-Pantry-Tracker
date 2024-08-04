d85b7354e8936803d80ea37f00427311a6e166a0
Brian <bw49078@gmail.com>
2024-08-04 00:10:03 -0500

    put env variables in its own file

--- app/firebase.js
+++ app/firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyACV8k5DJMRw6uA_AJ8Hv2cpoJuslclxvU",
-  authDomain: "ai-pantry-tracker-4b500.firebaseapp.com",
-  projectId: "ai-pantry-tracker-4b500",
-  storageBucket: "ai-pantry-tracker-4b500.appspot.com",
-  messagingSenderId: "1031813308600",
-  appId: "1:1031813308600:web:4afb785c3f88fe197b544d",
-  measurementId: "G-T2KHQCJZK9"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
